09dea87fb93069ae2054088edf932152f672fccc
Lukasz Karas <karasluka@gmail.com>
2018-10-10 17:55:13 +0200

    Initial commit

---
+++ src/main/java/pl/infoshare/App.java
@@ -0,0 +1,24 @@
+package pl.infoshare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String name = "Lukasz";
+        String lastname = "Karas";
+        System.out.println(name + " " + lastname);
+        byte age = 33;
+        String hometown = "Gdansk";
+        System.out.println(age + "\n" + hometown);
+        int result = 10 + 5;
+
+
+        System.out.println( "Hello World!" );
+
+    }
+}
+
